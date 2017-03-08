e33b77a262c9c80e5c5ea5a474dbba0654f15dda
ronizzle <roniel44@gmail.com>
2017-03-08 15:53:18 +0800

    hello world class + test class phpunit configuration with code coverage composer.json configuration

---
+++ test/HelloWorld.test.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ronizzle
+ * Date: 3/8/17
+ * Time: 2:54 PM
+ */
+class HelloWorldTest extends \PHPUnit_Framework_TestCase {
+    public function testTrueIsTrue() {
+        $this->assertTrue(true);
+    }
+}
